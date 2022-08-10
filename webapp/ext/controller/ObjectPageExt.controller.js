6240131a09e6a900c6776abfd187d9941aec1a48
Dzmitry Babichau <dzmitry.babichau@LeverX.local>
2022-08-10 15:25:36 +0300

    added custom button for create product without validation

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,20 +1,113 @@
-sap.ui.define([], function () {
-  "use strict";
-  return {
-    onAfterRendering: function () {
-      if (this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--ProductID::deleteEntry")) {
-        this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--ProductID::deleteEntry").setVisible(false);
-      }
+sap.ui.define(
+  ["sap/ui/model/json/JSONModel", "sap/m/MessageBox", "sap/m/MessageToast"],
+  function (JSONModel, MessageBox, MessageToast) {
+    "use strict";
 
-      setTimeout(()=>{
-        if (this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--delete")) {
-          this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--delete").setVisible(false);
+    var sDeleteProduct              = "project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--ProductID::deleteEntry";
+    var sDeleteProducts             = "project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--delete";
+    var sDeleteProductOnObjectPage  = "project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Products--delete";
+    var sCreateProduct              = "project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Categories--ProductID::addEntry";
+    return {
+      onAfterRendering: function () {
+        if (this.oView.byId(sDeleteProduct)) {
+          this.oView.byId(sDeleteProduct).setVisible(false);
         }
-      }, 0)
 
-      if (this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Products--delete")) {
-        this.oView.byId("project12::sap.suite.ui.generic.template.ObjectPage.view.Details::Products--delete").setVisible(false);
-      }
-    },
-  };
-});
+        setTimeout(() => {
+          if (this.oView.byId(sDeleteProducts)) {
+            this.oView.byId(sDeleteProducts).setVisible(false);
+          }
+        }, 0);
+
+        if (this.oView.byId(sDeleteProductOnObjectPage)) {
+          this.oView.byId(sDeleteProductOnObjectPage).setVisible(false);
+        }
+
+        if (this.oView.byId(sCreateProduct)) {
+          this.oView.byId(sCreateProduct).setVisible(false);
+        }
+      },
+
+      onInit: function () {
+        this.getOwnerComponent().getRouter().getRoute();
+      },
+
+      _setNewProductModel: function () {
+        var oNewProduct = new JSONModel({
+          Name: null,
+          Description: null,
+          ReleaseDate: null,
+          DiscontinuedDate: null,
+          Rating: 0,
+          Price: null,
+        });
+
+        this.getView().setModel(oNewProduct, "newProduct");
+      },
+
+      openDialog: function () {
+        this._setNewProductModel();
+        var oView = this.getView();
+
+        if (!this.oDialog) {
+          this.oDialog = sap.ui.xmlfragment(
+            oView.getId(),
+            `project12.ext.fragments.Dialog`,
+            this
+          );
+          oView.addDependent(this.oDialog);
+        }
+
+        this._setIDProduct();
+        this.oDialog.open();
+      },
+
+      _setIDProduct: function () {
+        var sURL = this.getOwnerComponent()
+          .getRouter()
+          .getHashChanger()
+          .getHash()
+          .split("/")[0];
+        var oNewProduct = this.getView().getModel("newProduct").oData;
+        var oODataModel = this.getView().getModel();
+
+        oODataModel.read(`/${sURL}/Products`, {
+          success: function (mData) {
+            oNewProduct.ID = mData.results.length;
+          },
+          error: function () {
+            MessageBox.error("Error!!!");
+          },
+        });
+      },
+
+      onCreateProductPress: function () {
+        var sURL = this.getOwnerComponent()
+          .getRouter()
+          .getHashChanger()
+          .getHash()
+          .split("/")[0];
+        var oNewProduct = this.getView().getModel("newProduct").oData;
+        var oODataModel = this.getView().getModel();
+
+        oODataModel.create(`/${sURL}/Products`, oNewProduct, {
+          success: function () {
+            MessageToast.show("Product was successfully created!");
+          },
+          error: function () {
+            MessageBox.error("Error while creating product!");
+          },
+        });
+
+        // oODataModel.refresh();
+        this.onDialogClosePress();
+      },
+
+      onDialogClosePress: function () {
+        var oODataModel = this.getView().getModel();
+        oODataModel.refresh();
+        this.oDialog.close();
+      },
+    };
+  }
+);
